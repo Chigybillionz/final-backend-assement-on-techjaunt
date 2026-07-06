bcee01f0421e6a6547f93801960a07dae181f94b
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2026-07-06 13:13:56 +0100

    feat(auth): create authentication module structure

---
+++ src/modules/auth/repositories/auth.repository.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../../../database/datasource";
+import { User } from "../../../entities/User.entity";
+
+export class AuthRepository {
+  private readonly repository = AppDataSource.getRepository(User);
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.repository.findOne({
+      where: { email },
+    });
+  }
+
+  async create(user: Partial<User>): Promise<User> {
+    const newUser = this.repository.create(user);
+    return this.repository.save(newUser);
+  }
+}
